6cc873fa7086a85d938e1080024e90902116361a
Garret Akpale <garretakpale@gmail.com>
2024-08-11 11:21:00 -0700

    server

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
+import controllerRouting from './routes/index';
 
 const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+
+controllerRouting(server);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 injectMiddlewares(server);
 injectRoutes(server);
